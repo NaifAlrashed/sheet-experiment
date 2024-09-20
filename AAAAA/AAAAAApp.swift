adac06c9465ef57253890c71cfc5346dfea84c51
Naif <naifdev@gmail.com>
2024-09-20 16:39:54 +0300

    Initial Commit

---
+++ AAAAA/AAAAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AAAAAApp.swift
+//  AAAAA
+//
+//  Created by Naif Alrashed on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AAAAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
